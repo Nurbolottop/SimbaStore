38cc03c6fc36e8ff1a9d7d20b6c30582897ad6e6
GeeksOsh2021 <geektechosh@gmail.com>
2024-01-07 18:56:50 -0500

    updpate

--- apps/secondary/views.py
+++ apps/secondary/views.py
@@ -1,7 +1,3 @@
 from django.shortcuts import render
-from apps.base import models
 
 # Create your views here.
-def index(request):
-    settings = models.Settings.objects.all()
-    return render(request,'base/index-2.html', request)
